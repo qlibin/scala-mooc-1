b241da1a0aeec91f414c762fe44faf2f1f92de5f
alibin <qlibin@gmail.com>
2016-11-12 00:11:49 +0300

    moved example to subdirectory initial assignment for week 1

---
+++ week1assignment/example/src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+import example.Lists
+import week1.Examples
+
+object Main extends App {
+
+  println(Examples.sqrt(2))
+  println(Examples.sqrt(9))
+  println(Examples.sqrt(1e-6))
+  println(Examples.sqrt(1e60))
+}
