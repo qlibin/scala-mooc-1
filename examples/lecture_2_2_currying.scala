932f72130cea05ff160383c2850bf36af1947285
alibin <qlibin@gmail.com>
2016-11-14 23:32:07 +0300

    Lecture 2.2 Currying

---
+++ examples/lecture_2_2_currying.scala
@@ -0,0 +1,14 @@
+
+def product(f: Int => Int)(a: Int, b: Int): Int =
+  if (a > b) 1
+  else f(a) * product(f)(a + 1, b)
+
+def fact(n: Int) = product(x=>x)(1, n)
+
+fact(4)
+
+def general(stopValue: Int, f: Int => Int, operation: (Int, Int) => Int)(a: Int, b: Int): Int =
+  if (a > b) stopValue
+  else operation(f(a), product(f)(a + 1, b))
+
+general(0, x => x, (x, y) => x + y)(1, 4)
