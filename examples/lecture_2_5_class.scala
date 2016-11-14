81146f12b33bf7434e0073a45d759c92866922b2
alibin <qlibin@gmail.com>
2016-11-15 00:42:29 +0300

    Lecture 2.5 more about class. constructors. argument validation

--- examples/lecture_2_5_class.scala
+++ examples/lecture_2_5_class.scala
@@ -1,25 +1,31 @@
 class Rational(x: Int, y: Int) {
+  require(y != 0, "Denominator must not be zero")
 
-  def numer = x
+  def this(x: Int) = this(x, 1)
 
-  def denom = y
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  private val g = gcd(x, y)
+  val numer = x / g
+  val denom = y / g
 
   def add(that: Rational) =
     new Rational(numer * that.denom + that.numer * denom,
       denom * that.denom)
-  
+
   def neg: Rational = new Rational(-x, y)
-  
+
   def sub(that: Rational) = add(that.neg)
-  
+
   def mul(that: Rational) =
-    new Rational(numer * that.numer,
-      denom * that.denom)
-  
+    new Rational(numer * that.numer, denom * that.denom)
+
   def div(that: Rational) =
-    new Rational(numer * that.denom,
-      denom * that.numer)
-  
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def less(than: Rational) = numer * than.denom < than.numer * denom
+
+  def max(that: Rational) = if (this.less(that)) that else this
+
   override def toString = s"$numer/$denom"
 }
 
@@ -31,6 +37,8 @@ val y = new Rational(2, 3)
 
 x.add(y).sub(x).neg
 
+y.add(y)
+
 val a = new Rational(1, 3)
 val b = new Rational(5, 7)
 val c = new Rational(3, 2)
@@ -38,3 +46,12 @@ val c = new Rational(3, 2)
 a.sub(b).sub(c)
 
 a.add(b).mul(c)
+
+x.less(y)
+
+y.less(x)
+
+x.max(y)
+y.max(x)
+
+assert(y.max(x).numer == 2, "!")
