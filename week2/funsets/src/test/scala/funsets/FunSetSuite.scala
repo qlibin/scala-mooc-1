2e5f6dba43c74216674f3cf02e0b798230220c5f
alibin <qlibin@gmail.com>
2016-11-19 16:45:09 +0300

    Week 2 assignment: 2.2 Sets 2 exists implementation

--- week2/funsets/src/test/scala/funsets/FunSetSuite.scala
+++ week2/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -167,7 +167,6 @@ class FunSetSuite extends FunSuite {
 
   test("forall works as expected") {
     new TestSets {
-      forall(even, (x: Int) => x > -10 && x < 10)
       assert(!forall(even, (x: Int) => x > -10 && x < 10))
       assert(!forall(odd, (x: Int) => x > -10 && x < 10))
       assert(!forall(rating10, (x: Int) => x > 5 && x < 10))
@@ -175,6 +174,25 @@ class FunSetSuite extends FunSuite {
       assert(forall(negative, (x: Int) => x < 0))
       assert(forall(positive, (x: Int) => x >= 0))
       assert(!forall(positive, (x: Int) => x > 1))
+    }
+  }
+
+  test("exists works as expected") {
+    new TestSets {
+      assert(exists(even, x => x == -10))
+      assert(!exists(odd, x => x == -10))
+      assert(exists(even, x => x == 1000))
+      assert(exists(even, x => x == -1000))
+      assert(exists(odd, x => x == 999))
+      assert(exists(odd, x => x == -999))
+      assert(exists(even, x => x > -10 && x < 10))
+      assert(exists(odd, x => x > -10 && x < 10))
+      assert(exists(rating10, x => x > 5 && x < 10))
+      assert(exists(rating10, x => x > -5 && x < 100))
+      assert(exists(negative, x => x < 0))
+      assert(exists(positive, x => x >= 0))
+      assert(!exists(positive, x => x < 0))
+      assert(exists(positive, x => x > 1))
     }
   }
 
